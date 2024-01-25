a4f04881bfea238535318dbba6e5503bfaee6cd2
ManikantaPinabakala <manikantapinabakala@gmail.com>
2024-01-25 14:00:36 +0000

    first updates

---
+++ src/components/TabItem/index.js
@@ -0,0 +1,24 @@
+import './index.css'
+
+const TabItem = props => {
+  const {tabDetails, updateActiveTabId, isActive} = props
+  const {tabId, displayText} = tabDetails
+  const updateTabId = () => {
+    updateActiveTabId(tabId)
+  }
+  const activeClassName = isActive ? 'active-tab-btn' : ''
+
+  return (
+    <li className="tab-item-container ">
+      <button
+        type="button"
+        className={`tab-btn ${activeClassName}`}
+        onClick={updateTabId}
+      >
+        {displayText}
+      </button>
+    </li>
+  )
+}
+
+export default TabItem
